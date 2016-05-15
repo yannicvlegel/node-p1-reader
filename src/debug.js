6779003fab8a548bc89cba3170861066ad662f38
Ruud Verheijden <ruud.verheijden@gmail.com>
2016-05-15 16:24:42 +0200

    Moving debug mode logic to separate file

---
+++ src/debug.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var config = require('./config/config.json');
+
+/**
+ * Write raw and parsed data to log file for debugging purposes
+ *
+ * @param packet : P1 packet according to DSMR4.0 specification
+ */
+function writeToLogFile(rawPacket, parsedPacket) {
+    var now = new Date().toUTCString();
+
+    fs.appendFile(config.debugRawDataFile, 'Package received at ' + now + ':\n' + rawPacket + '\n\n', function (err) {
+        if (err) {
+            console.error('Could not write raw package to ' + config.debugRawDataFile);
+        }
+    });
+
+    fs.appendFile(config.debugParsedDataFile, 'Package received at ' + now + ':\n' + JSON.stringify(parsedPacket, true, 4) + '\n\n', function (err) {
+        if (err) {
+            console.error('Could not write parsed package to ' + config.debugParsedDataFile);
+        }
+    });
+}
+
+module.exports.writeToLogFile = writeToLogFile;
