2676ba32a7c297c36698ad91392e0bb9a07bf4c9
Ruud Verheijden <ruud.verheijden@gmail.com>
2019-09-14 15:48:42 +0200

    Add parsing of DSMR2.2 packets and updating the tests accordingly

--- spec/mainSpec.js
+++ spec/mainSpec.js
@@ -1,10 +1,10 @@
 describe("parsePacket", function() {
-    var parsePacket = require('../lib/parsePacket');
+    const parsePacket = require('../lib/parsePacket');
 
-    var defaultHeader = "/MyMeterType\r\n \r\n";
+    const defaultHeader = "/MyMeterType\r\n \r\n";
 
     it("should be able to parse the packet header containing the meter type", function() {
-        var parsedPacket = parsePacket(defaultHeader);
+        const parsedPacket = parsePacket(defaultHeader);
 
         expect(parsedPacket.meterType).toEqual("MyMeterType");
     });
--- spec/parsePacketSpec.js
+++ spec/parsePacketSpec.js
@@ -10,11 +10,12 @@ describe("parsePacket", function() {
     });
 
     it("should return the expected output object", function() {
-        const parsedPacket = parsePacket(defaultHeader);
+        // Input the default header and set version to "00", to prevent the incorrect assumption of a DSMR2.2 packet
+        const parsedPacket = parsePacket(defaultHeader + "1-3:0.2.8(00)");
 
         const expectedOutputObject = {
             meterType: "MyMeterType",
-                version: null,
+                version: "00",
                 timestamp: null,
                 equipmentId: null,
                 textMessage: {
@@ -127,14 +128,16 @@ describe("parsePacket", function() {
     });
 
     it("should be able to parse the timestamp (summer time) from a packet", function() {
-        const packet = defaultHeader + "0-0:1.0.0(160520213143S)";
+        // Input the default header and set version to "00", to prevent the incorrect assumption of a DSMR2.2 packet
+        const packet = defaultHeader + "1-3:0.2.8(00)\r\n" + "0-0:1.0.0(160520213143S)";
         const parsedPacket = parsePacket(packet);
 
         expect(parsedPacket.timestamp).toEqual("2016-05-20T19:31:43.000Z");
     });
 
     it("should be able to parse the timestamp (winter time) from a packet", function() {
-        const packet = defaultHeader + "0-0:1.0.0(160100000000W)";
+        // Input the default header and set version to "00", to prevent the incorrect assumption of a DSMR2.2 packet
+        const packet = defaultHeader + "1-3:0.2.8(00)\r\n" + "0-0:1.0.0(160100000000W)";
         const parsedPacket = parsePacket(packet);
 
         expect(parsedPacket.timestamp).toEqual("2015-12-30T23:00:00.000Z");
@@ -302,7 +305,309 @@ describe("parsePacket", function() {
         expect(parsedPacket).toEqual(expectedOutputObject);
     });
 
-    it("should be able to parse a complete packet (official documentation example)", function() {
+    it("should be able to parse an example DSMR4.0 packet of a KAIFA E0003 meter", function() {
+        const packet = defaultHeader +
+            "1-3:0.2.8(42)\r\n" +
+            "0-0:1.0.0(160520213143S)\r\n" +
+            "0-0:96.1.1(1234567890123456789012345678901234)\r\n" +
+            "1-0:1.8.1(000123.456*kWh)\r\n" +
+            "1-0:1.8.2(000456.789*kWh)\r\n" +
+            "1-0:2.8.1(000200.120*kWh)\r\n" +
+            "1-0:2.8.2(000300.230*kWh)\r\n" +
+            "0-0:96.14.0(0001)\r\n" +
+            "1-0:1.7.0(00.345*kW)\r\n" +
+            "1-0:2.7.0(00.456*kW)\r\n" +
+            "0-0:96.7.21(00008)\r\n" +
+            "0-0:96.7.9(00005)\r\n" +
+            "1-0:99.97.0(1)(0-0:96.7.19)(000101000014W)(60*s)\r\n" +
+            "1-0:32.32.0(00001)\r\n" +
+            "1-0:52.32.0(00002)\r\n" +
+            "1-0:72.32.0(00003)\r\n" +
+            "1-0:32.36.0(00004)\r\n" +
+            "1-0:52.36.0(00005)\r\n" +
+            "1-0:72.36.0(00006)\r\n" +
+            "0-0:96.13.1(12345678)\r\n" +
+            "0-0:96.13.0(303132333435363738393A3B3C3D3E3F)\r\n" +
+            "1-0:31.7.0(001*A)\r\n" +
+            "1-0:51.7.0(002*A)\r\n" +
+            "1-0:71.7.0(003*A)\r\n" +
+            "1-0:21.7.0(00.201*kW)\r\n" +
+            "1-0:22.7.0(00.222*kW)\r\n" +
+            "1-0:41.7.0(00.401*kW)\r\n" +
+            "1-0:42.7.0(00.422*kW)\r\n" +
+            "1-0:61.7.0(00.601*kW)\r\n" +
+            "1-0:62.7.0(00.622*kW)\r\n" +
+            "0-1:24.1.0(003)\r\n" +
+            "0-1:96.1.0(1234567890123456789012345678901234)\r\n" +
+            "0-1:24.2.1(160520210000S)(00500.123*m3)";
+
+        const parsedPacket = parsePacket(packet);
+        
+        const expectedOutputObject = {
+            "meterType": "MyMeterType",
+            "version": "42",
+            "timestamp": "2016-05-20T19:31:43.000Z",
+            "equipmentId": "1234567890123456789012345678901234",
+            "textMessage": {
+                "codes": "12345678",
+                "message": "0123456789:;<=>?"
+            },
+            "electricity": {
+                "received": {
+                    "tariff1": {
+                        "reading": 123.456,
+                        "unit": "kWh"
+                    },
+                    "tariff2": {
+                        "reading": 456.789,
+                        "unit": "kWh"
+                    },
+                    "actual": {
+                        "reading": 0.345,
+                        "unit": "kW"
+                    }
+                },
+                "delivered": {
+                    "tariff1": {
+                        "reading": 200.12,
+                        "unit": "kWh"
+                    },
+                    "tariff2": {
+                        "reading": 300.23,
+                        "unit": "kWh"
+                    },
+                    "actual": {
+                        "reading": 0.456,
+                        "unit": "kW"
+                    }
+                },
+                "tariffIndicator": 1,
+                "threshold": null,
+                "switchPosition": null,
+                "numberOfPowerFailures": 8,
+                "numberOfLongPowerFailures": 5,
+                "longPowerFailureLog": {
+                    "count": 1,
+                    "log": [
+                        {
+                            "startOfFailure": "1999-12-31T22:59:14.000Z",
+                            "endOfFailure": "1999-12-31T23:00:14.000Z",
+                            "duration": 60,
+                            "unit": "s"
+                        }
+                    ]
+                },
+                "voltageSags": {
+                    "L1": 1,
+                    "L2": 2,
+                    "L3": 3
+                },
+                "voltageSwell": {
+                    "L1": 4,
+                    "L2": 5,
+                    "L3": 6
+                },
+                "instantaneous": {
+                    "current": {
+                        "L1": {
+                            "reading": 1,
+                            "unit": "A"
+                        },
+                        "L2": {
+                            "reading": 2,
+                            "unit": "A"
+                        },
+                        "L3": {
+                            "reading": 3,
+                            "unit": "A"
+                        }
+                    },
+                    "power": {
+                        "positive": {
+                            "L1": {
+                                "reading": 0.201,
+                                "unit": "kW"
+                            },
+                            "L2": {
+                                "reading": 0.401,
+                                "unit": "kW"
+                            },
+                            "L3": {
+                                "reading": 0.601,
+                                "unit": "kW"
+                            }
+                        },
+                        "negative": {
+                            "L1": {
+                                "reading": 0.222,
+                                "unit": "kW"
+                            },
+                            "L2": {
+                                "reading": 0.422,
+                                "unit": "kW"
+                            },
+                            "L3": {
+                                "reading": 0.622,
+                                "unit": "kW"
+                            }
+                        }
+                    }
+                }
+            },
+            "gas": {
+                "deviceType": "003",
+                "equipmentId": "1234567890123456789012345678901234",
+                "timestamp": "2016-05-20T19:00:00.000Z",
+                "reading": 500.123,
+                "unit": "m3",
+                "valvePosition": null
+            }
+        };
+
+        expect(parsedPacket).toEqual(expectedOutputObject);
+    });
+
+    it("should be able to parse an example DSMR2.2 packet of a Kamstrup 162 meter", function() {
+        const packet = "/KMP5 ABCD123456789012\r\n" +
+        "0-0:96.1.1(1234567890123456789012345678901234)\r\n" +
+        "1-0:1.8.1(01234.567*kWh)\r\n" +
+        "1-0:1.8.2(02345.678*kWh)\r\n" +
+        "1-0:2.8.1(03456.789*kWh)\r\n" +
+        "1-0:2.8.2(04567.890*kWh)\r\n" +
+        "0-0:96.14.0(0001)\r\n" +
+        "1-0:1.7.0(0001.23*kW)\r\n" +
+        "1-0:2.7.0(0002.34*kW)\r\n" +
+        "0-0:96.13.1(12345678)\r\n" +
+        "0-0:96.13.0(303132333435363738393A3B3C3D3E3F)\r\n" +
+        "0-1:24.1.0(3)\r\n" +
+        "0-1:96.1.0(2345678901234567890123456789012345)\r\n" +
+        "0-1:24.3.0(190914100000)(08)(60)(1)(0-1:24.2.1)(m3)\r\n" +
+        "(02888.297)\r\n";
+
+        const parsedPacket = parsePacket(packet);
+
+        console.log(parsedPacket);
+        
+        const expectedOutputObject = {
+            "meterType": "KMP5 ABCD123456789012",
+            "version": "22",
+            "timestamp": "2016-05-20T19:31:43.000Z",
+            "equipmentId": "1234567890123456789012345678901234",
+            "textMessage": {
+                "codes": "12345678",
+                "message": "0123456789:;<=>?"
+            },
+            "electricity": {
+                "received": {
+                    "tariff1": {
+                        "reading": 1234.567,
+                        "unit": "kWh"
+                    },
+                    "tariff2": {
+                        "reading": 2345.678,
+                        "unit": "kWh"
+                    },
+                    "actual": {
+                        "reading": 1.23,
+                        "unit": "kW"
+                    }
+                },
+                "delivered": {
+                    "tariff1": {
+                        "reading": 3456.789,
+                        "unit": "kWh"
+                    },
+                    "tariff2": {
+                        "reading": 4567.890,
+                        "unit": "kWh"
+                    },
+                    "actual": {
+                        "reading": 2.34,
+                        "unit": "kW"
+                    }
+                },
+                "tariffIndicator": 1,
+                "threshold": null,
+                "switchPosition": null,
+                "numberOfPowerFailures": null,
+                "numberOfLongPowerFailures": null,
+                "longPowerFailureLog": null,
+                "voltageSags": {
+                    "L1": null,
+                    "L2": null,
+                    "L3": null
+                },
+                "voltageSwell": {
+                    "L1": null,
+                    "L2": null,
+                    "L3": null
+                },
+                "instantaneous": {
+                    "current": {
+                        "L1": {
+                            "reading": null,
+                            "unit": null
+                        },
+                        "L2": {
+                            "reading": null,
+                            "unit": null
+                        },
+                        "L3": {
+                            "reading": null,
+                            "unit": null
+                        }
+                    },
+                    "power": {
+                        "positive": {
+                            "L1": {
+                                "reading": null,
+                                "unit": null
+                            },
+                            "L2": {
+                                "reading": null,
+                                "unit": null
+                            },
+                            "L3": {
+                                "reading": null,
+                                "unit": null
+                            }
+                        },
+                        "negative": {
+                            "L1": {
+                                "reading": null,
+                                "unit": null
+                            },
+                            "L2": {
+                                "reading": null,
+                                "unit": null
+                            },
+                            "L3": {
+                                "reading": null,
+                                "unit": null
+                            }
+                        }
+                    }
+                }
+            },
+            "gas": {
+                "deviceType": "3",
+                "equipmentId": "2345678901234567890123456789012345",
+                "timestamp": "2019-09-14T08:00:00.000Z",
+                "reading": 2888.297,
+                "unit": "m3",
+                "valvePosition": null
+            }
+        };
+
+        // Set timestamp of the expected output to now since for DSMR2.2 we the server determines the timestamp
+        const now = new Date();
+        now.setMilliseconds(0);
+        expectedOutputObject.timestamp = now.toISOString();
+
+        expect(parsedPacket).toEqual(expectedOutputObject);
+    });
+
+    it("should be able to parse a complete DSMR4.0 packet (official documentation example)", function() {
         const packet = "/ISk5\\2MT382-1000\r\n \r\n" +
             "1-3:0.2.8(42)\r\n" +
             "0-0:1.0.0(101209113020W)\r\n" +
